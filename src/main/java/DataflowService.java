de409199836065773ae0459796e00660a495472e
hackee12 <hackee.owl@gmail.com>
2022-04-04 01:35:08 -0400

    Enable job status check

--- src/main/java/DataflowService.java
+++ src/main/java/DataflowService.java
@@ -1,13 +1,28 @@
 import com.google.api.services.dataflow.Dataflow;
 import com.google.api.services.dataflow.model.Job;
+import com.google.dataflow.v1beta3.JobState;
 import lombok.AllArgsConstructor;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @AllArgsConstructor
 public class DataflowService {
+    private static final Set<String> JOB_STATUS_FAILURE = Stream.of(
+                    JobState.JOB_STATE_CANCELLED,
+                    JobState.JOB_STATE_FAILED,
+                    JobState.JOB_STATE_DRAINED,
+                    JobState.JOB_STATE_UNKNOWN,
+                    JobState.UNRECOGNIZED
+            )
+            .map(Object::toString)
+            .collect(Collectors.toSet());
+
+    private static final String JOB_STATUS_SUCCESS = JobState.JOB_STATE_DONE.toString();
+
     private final Dataflow dataflow;
     private final ResourceBoundary boundary;
 
@@ -28,9 +43,18 @@ public class DataflowService {
                 .execute();
     }
 
-    public List<Job> getJobByName(String jobName) throws IOException {
+    public Job getJobByName(String jobName) throws IOException {
         return listAllJobs().stream()
                 .filter(job -> job.getName().equals(jobName))
-                .collect(Collectors.toList());
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(String.format("Can't find job by name %s.", jobName)));
+    }
+
+    public boolean isJobStatusSuccess(Job job) {
+        return JOB_STATUS_SUCCESS.equals(job.getName());
+    }
+
+    public boolean isJobStatusFailure(Job job) {
+        return JOB_STATUS_FAILURE.contains(job.getCurrentState());
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,9 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.dataflow.Dataflow;
-import com.google.api.services.dataflow.model.Job;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.List;
-import java.util.Objects;
-
-import static java.util.Objects.isNull;
 
 public class Main {
 
@@ -26,15 +21,12 @@ public class Main {
                 GoogleCredential.getApplicationDefault()
         );
 
-        var service = new DataflowService(dataflow, new ResourceBoundary(projectId, region));
-
-        List<Job> allJobs = service.listAllJobs();
-
-        if (isNull(service.getJobById(allJobs.get(0).getId())) ) {
-            throw new RuntimeException("Can't find job by id.");
-        }
-        if (isNull(service.getJobByName(allJobs.get(0).getName()))) {
-            throw new RuntimeException("Can't find job by name.");
-        }
+        final DataflowService service = new DataflowService(dataflow, new ResourceBoundary(projectId, region));
+        var allJobs = service.listAllJobs();
+        var job = allJobs.get(0);
+        var id = job.getId();
+        var name = job.getName();
+        service.isJobStatusFailure(service.getJobById(id));
+        service.isJobStatusSuccess(service.getJobByName(name));
     }
 }
