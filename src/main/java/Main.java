5b19aad010f81dab71e4c279144137f11dd7f84d
hackee12 <hackee.owl@gmail.com>
2022-04-07 17:29:13 -0400

    Straighten usage example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.dataflow.Dataflow;
+import com.google.api.services.dataflow.model.Job;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Optional;
 
 import static java.lang.String.format;
 
@@ -31,13 +33,8 @@ public class Main {
         System.out.println("Is job status success: " + service.isJobStatusSuccess(someJob));
         System.out.println("Is job status failure: " + service.isJobStatusFailure(someJob));
 
-        var validJobName = someJob.getName();
-        service.getJobByName(validJobName)
-                .orElseThrow(() -> new RuntimeException(format("Can't find job by name [%s].", validJobName)));
-
-        var validJobId = someJob.getId();
-        service.getJobByName(validJobName)
-                .orElseThrow(() -> new RuntimeException(format("Can't find job by id [%s].", validJobId)));
+        Optional<Job> jobByName = service.getJobByName(someJob.getName());
+        Job jobById = service.getJobById(someJob.getId());
 
         var invalidJobName = "xxx";
         service.getJobByName(invalidJobName)
